7bb4c2ecba949ce7aaa9409c02255545b9730d04
Matt Cowan <matt@mattbcowan.com>
2022-05-04 06:35:33 -0500

    feat: Add playRound function
    Add a playRound function that takes the userSelection and
    computerSelection and verifies who won the round. It then
    returns a string with who won and why they won.

--- script.js
+++ script.js
@@ -3,8 +3,46 @@
 // rock, paper or scissors and return the value
 
 const choices = ["Rock", "Paper", "Scissors"];
+let userChoice = prompt("Choose rock, paper or  scissors: ");
 
 const computerPlay = () => {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 };
+
+// Write a function named playRound that takes
+// the user selection and the random selection
+// from the computer and RETURNS a string.
+const playRound = (userSelection, computerSelection) => {
+  // NOTE: Player choice must be case insensitive
+  userSelection = userSelection
+    .trim()
+    .toLowerCase()
+    .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+
+  if (
+    (userSelection === "Rock" && computerSelection === "Paper") ||
+    (userSelection === "Paper" && computerSelection === "Scissors") ||
+    (userSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    // If the user loses write:
+    // "You Lose! Paper beats Rock"
+    // replacing paper and rock with the
+    // correct choices.
+    return `You Lose! ${computerSelection} beats ${userSelection}`;
+  } else if (
+    (userSelection === "Paper" && computerSelection === "Rock") ||
+    (userSelection === "Scissors" && computerSelection === "Paper") ||
+    (userSelection === "Rock" && computerSelection === "Scissors")
+  ) {
+    // If the user wins write:
+    // "You Win! Scissors beats Paper!"
+    // replacing scissors and paper with
+    // the correct choices
+    return `You Win! ${userSelection} beats ${computerSelection}`;
+  } else {
+    return "It's a tie!";
+  }
+};
+
+console.log(playRound(userChoice, computerPlay()));
