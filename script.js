d0333463a0c8ab6f660dc31908a6cb9593496ce4
Matt Cowan <matt@mattbcowan.com>
2022-05-04 06:21:35 -0500

    feat: Add computerPlay function
    Add the computerPlay function that allows the computer
    to choose between 3 choices (Rock, Paper, or Scissors)
    and returns the result.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("hello");
+// Begin with a function called computerPlay
+// Function should choose a random choice between
+// rock, paper or scissors and return the value
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+const computerPlay = () => {
+  let choice = Math.floor(Math.random() * 3);
+  return choices[choice];
+};
